fb487228b9d9f3bee6fffc22525ccc6fc7f14dda
Pedro Folim <pedro.folim@fons.com.br>
2023-01-19 12:13:34 -0800

    fix: teste

--- orchestrate/dags/covid19-dag.py
+++ orchestrate/dags/covid19-dag.py
@@ -6,7 +6,7 @@ from airflow.operators.bash import BashOperator
 
 with DAG(
     dag_id='dbt_dag',
-    start_date=datetime(2021, 12, 23),
+    start_date=datetime(2022, 1, 19),
     description='An Airflow DAG to invoke simple dbt commands',
     schedule=timedelta(days=1),
 ) as dag:
