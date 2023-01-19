9b2a8a2596370aaea659428b890744ccb990549b
Pedro Folim <pedro.folim@fons.com.br>
2023-01-19 12:24:05 -0800

    fix: update

--- orchestrate/dags/covid19-dag.py
+++ orchestrate/dags/covid19-dag.py
@@ -1,24 +1,47 @@
-from datetime import datetime, timedelta
+import os
+
+import pendulum
 
 from airflow import DAG
+from airflow.decorators import task
 from airflow.operators.bash import BashOperator
 
+from kubernetes.client import models as k8s
+
+default_args = {
+    'owner': 'airflow',
+    'email': 'sebastian@convexa.ai',
+    'email_on_failure': True
+}
 
 with DAG(
-    dag_id='dbt_dag',
-    start_date=datetime(2022, 1, 19),
-    description='An Airflow DAG to invoke simple dbt commands',
-    schedule=timedelta(days=1),
+    dag_id="test_pandas",
+    default_args=default_args,
+    start_date=pendulum.datetime(2021, 1, 1, tz="UTC"),
+    catchup=False,
+    tags=["sample_tag"],
 ) as dag:
+    executor_config_template = {
+        "pod_override": k8s.V1Pod(
+            spec=k8s.V1PodSpec(
+                containers=[
+                    k8s.V1Container(
+                        name="base", image="datacoves/airflow-pandas:latest"
+                    )
+                ]
+            )
+        ),
+    }
 
-    dbt_run = BashOperator(
-        task_id='dbt_run',
-        bash_command='dbt run'
+    task_x = BashOperator(
+        task_id="bash_executor_config",
+        executor_config=executor_config_template,
+        bash_command="echo SUCCESS",
     )
 
-    dbt_test = BashOperator(
-        task_id='dbt_test',
-        bash_command='dbt test'
+    fail = BashOperator(
+        task_id='failing',
+        bash_command="dates"
     )
 
-    dbt_run >> dbt_test
+    task_x >> fail
